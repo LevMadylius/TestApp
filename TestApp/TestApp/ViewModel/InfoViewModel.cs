84e6814253d7c6189b9ee36850804393d755db84
Lev Madylius <lev.madylius@gmail.com>
2018-02-10 20:25:28 +0200

    Removed unnecessary usings

--- TestApp/TestApp/ViewModel/InfoViewModel.cs
+++ TestApp/TestApp/ViewModel/InfoViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using TestApp.Model;
+﻿using TestApp.Model;
 
 namespace TestApp.ViewModel
 {
--- TestApp/TestApp/ViewModel/ItemViewModel.cs
+++ TestApp/TestApp/ViewModel/ItemViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using TestApp.Model;
+﻿using TestApp.Model;
 
 namespace TestApp.ViewModel
 {
