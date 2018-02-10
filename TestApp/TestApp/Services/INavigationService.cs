6005557a8b7414ef9c0d49c7f5b32abab1408b0e
Lev Madylius <lev.madylius@gmail.com>
2018-02-10 15:11:53 +0200

    Navigation Service implemented

---
+++ TestApp/TestApp/Services/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Services
+{
+    public interface INavigationService
+    {
+        void NavigateToContent(string url);
+        void NavigateToInfo<T>(T info);
+        void NavigateBack();
+    }
+}
---
+++ TestApp/TestApp/Services/NavigationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+namespace TestApp.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public async void NavigateBack()
+        {
+            var currentPage = GetCurrentPage();
+
+            await currentPage.Navigation.PopModalAsync();
+        }
+
+        public async void NavigateToContent(string url)
+        {
+            var currentPage = GetCurrentPage();
+            // implement constructor with params
+            await currentPage.Navigation.PushModalAsync(new MainPage(url));
+        }
+
+        public async void NavigateToInfo<T>(T info)
+        {
+            var currentPage = GetCurrentPage();
+            // implement constructor with params
+            await currentPage.Navigation.PushModalAsync(new InfoPage(info));
+        }
+
+        private Page GetCurrentPage()
+        {
+            Page currentPage = Application.Current.MainPage.Navigation.NavigationStack.LastOrDefault();
+
+            return currentPage;
+        }
+    }
+}
